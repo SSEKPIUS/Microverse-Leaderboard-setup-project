9cda23a3a542a7a553fb189e57ce002e3280271d
pius ssekweyama <ssekweyamapius@gmail.com>
2022-11-22 18:38:42 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.config.js');
+
+const compiler = webpack(config);
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+  }),
+);
+
+// Serve the files on port 3000.
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!\n');
+});
